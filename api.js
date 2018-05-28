97a988817a2a7f88eaab1c6df26c1647a6a57f68
guillermitus12 <guillermopacheco.exa@gmail.com>
2018-05-28 10:07:32 -0300

    cambio de ip

--- api.js
+++ api.js
@@ -77,7 +77,7 @@ app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
    var host = server.address().address
    var port = server.address().port
    
